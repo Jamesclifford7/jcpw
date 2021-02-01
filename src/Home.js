ee6a4d27f97ed357065b5cd56689cec7268ca4d1
James Clifford <jamesclifford7@gmail.com>
2021-01-31 22:04:04 -0800

    added skills, projects, and contact sections

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ import html5logo from './Images/HTML5_Logo_512.png'
 import css3logo from './Images/CSS3_logo_and_wordmark.svg.png'
 import javascriptlogo from './Images/JavaScript-Logo3.png'
 import reactlogo from './Images/react_logo2.png'
+import musiconnectimage from './Images/musiconnect-photo1.png'
 
 function Home() {
     return (
@@ -18,7 +19,7 @@ function Home() {
                     <p>Hi! I'm James, a front end developer based in San Diego, California. As a creative
                         person, I enjoy working on visually intriguing web applications with interesting
                         and dynamic functionalities. Services I can provide include HTML, CSS, JavaScript, and React.js. 
-                        My passion is using these tools to build sites with a fluid user interface, a distinct aesthetic, 
+                        My passion is using these tools to build sites with a fluid and responsive user interface, a distinct aesthetic, 
                         and bullet-proof utilities. Furthermore, I have experience working as a freelance copywriter for 
                         a variety of clients. I have written copy for several criminal defense attorneys, a luxury 
                         watch brand, a CBD company, and most recently a professional coaching startup, among others.
@@ -27,36 +28,36 @@ function Home() {
                 <div className="services">
                     <h1>Skills</h1>
                     <div className="services-container">
-                        
-                            <img src={html5logo} alt="html5 logo" />
-                        
-                    
-                            <img src={css3logo} alt="css3 logo" />
-                        
-                        
-                            <img src={javascriptlogo} alt="javascript logo" />
-                    
-                        
-                            <img src={reactlogo} alt="react logo" />
-        
-                    </div>
-                </div>
-                {/*
-                <div className="services-container">
-                    <div className="html">
                         <img src={html5logo} alt="html5 logo" />
-                    </div>
-                    <div className="css">
                         <img src={css3logo} alt="css3 logo" />
-                    </div>
-                    <div className="javascript">
                         <img src={javascriptlogo} alt="javascript logo" />
-                    </div>
-                    <div className="react">
                         <img src={reactlogo} alt="react logo" />
                     </div>
                 </div>
-                */}
+                <div id="projects">
+                    <h1>Projects</h1>
+                    <div className="projects-container">
+                        <div>
+                            <img src={musiconnectimage} alt="musiconnect landing page"/>
+                            <h2>Musiconnect</h2>
+                            <a href="https://musiconnect-app.vercel.app/" target="_blank" rel="noreferrer">https://musiconnect-app.vercel.app/</a>
+                            <p>A platform that enables musicians to connect with other musicians in their city. Sign up, create a profile, search, and message other users to start your next band. Built using React.js., Node.js, and PostgreSQL. </p>
+                        </div>
+                    </div>
+                </div>
+                <div id="contact">
+                    <h1>Contact</h1>
+                    <p>Drop me a line!</p>
+                    <form action="https://formspree.io/xgenrlaq" method="POST">
+                        <label htmlFor="name">Name:</label><br/>
+                        <input type="text" id="name" name="name" required></input><br />
+                        <label htmlFor="email">Email:</label><br />
+                        <input type="text" id="email" name="email" required></input><br />
+                        <label htmlFor="message">Message:</label><br />
+                        <textarea id="message" name="message" required></textarea><br />
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
             </div>
         </>
     )
--- src/Nav.js
+++ src/Nav.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { Link } from 'react-scroll'
 
 function Nav() {
     return (
         <header>    
             <nav>
                 <ul>
-                    <Link to="/"><li>Home</li></Link>
-                    <Link to="/projects"><li>Projects</li></Link>
-                    <Link to="/contact"><li>Contact</li></Link>
-                    <li><FontAwesomeIcon icon={faGithub} /></li>
+                    {/* <li><Link to="/">Home</Link></li> */}
+                    <li><Link to="projects" smooth={true} duration={1000}>Projects</Link></li>
+                    <li><Link to="contact" smooth={true} duration={1000}>Contact</Link></li>
+                    <li><a href="https://github.com/Jamesclifford7" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a></li>
                 </ul>
             </nav>
         </header>
